ddc731b74235887bd6bcfc8e487ad059271fa6b2
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2018-10-06 23:27:11 +0900

    오늘/내일/모레 급식

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ var auth = require('./auth.json');
 var cheerio = require('cheerio');
 var request = require('request');
 var moment = require('moment');
+var fs = require('fs');
 
 // Configure logger settings
 logger.remove(logger.transports.Console);
@@ -12,19 +13,40 @@ logger.add(logger.transports.Console, {
 });
 logger.level = 'debug';
 
-
 // Initialize Discord Bot
 var bot = new Discord.Client({
     token: auth.token,
     autorun: true
 });
 
-
 bot.on('ready', function (evt) {
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
+
+function getMeal(name, code, day, channelID){
+    request('https://stu.goe.go.kr/sts_sci_md01_001.do?schulCode=' 
+        + code + '&&schulCrseScCode=3&schMmealScCode=2&schYmd=' 
+        + moment().add(day, 'days').format('YYYY.MM.DD')
+    , function(error, response, html){
+        var $ = cheerio.load(html, { decodeEntities: false });
+        var meal = $('tbody > tr:nth-child(2)').children('td').eq(moment().add(day, 'days').day());
+        if (meal.html() == ' '){ bot.sendMessage({ to: channelID, message: '급식 정보가 없습니다.' }); }
+        else { bot.sendMessage({ to: channelID, message: meal.html() }); }
+    });
+    var obj = JSON.parse(fs.readFileSync('code.json', 'utf8'));
+    if (!obj.hasOwnProperty(name)) {
+        obj[name] = code;
+        const ordered = {};
+        Object.keys(obj).sort().forEach(function(key) {
+            ordered[key] = obj[key];
+        });
+        fs.writeFile('code.json', JSON.stringify(ordered), function(err) {
+            if (err) { console.log(err); }
+        }); 
+    } 
+}
 
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it needs to execute a command
@@ -38,42 +60,32 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         switch(cmd) {
             // !ping
             case '급식':
-                var fs = require('fs');
                 var obj = JSON.parse(fs.readFileSync('code.json', 'utf8'));
+                if (!['오늘', '내일', '모레'].includes(args[1])) {
+                    bot.sendMessage({ to: channelID, message: '`!급식 [학교이름] [오늘/내일/모레]` 형태로 입력해 줘!' });
+                    break;
+                }
+                var day = {'오늘':0, '내일':1, '모레':2}[args[1]]
                 if (obj.hasOwnProperty(args[0])) {
-                    var code = obj[args[0]]
-                    var day = args[1]
-                    request('https://stu.goe.go.kr/sts_sci_md01_001.do?schulCode=' + code + '&&schulCrseScCode=3&schMmealScCode=2&schYmd=' + moment().format('YYYY.MM.DD'), function(error, response, html){
-                        var $ = cheerio.load(html, { decodeEntities: false });
-                        var meal = $('tbody > tr:nth-child(2)').children('td').eq(day)
-                        bot.sendMessage({ to: channelID, message: meal.html() })
-                    });
+                    getMeal(args[0], obj[args[0]], day, channelID);
                 }
                 else {
+                    logger.info('query : ' + args[0])
                     request.post({
                         url: 'https://www.meatwatch.go.kr/biz/bm/sel/schoolListPopup.do',
                         form: {'criteria': 'pageIndex=1&bsnmNm=' + encodeURIComponent(args[0])}
                     }, function(error, response, body){
                         var $ = cheerio.load(body, { decodeEntities: false });
-                        var code = $('.grid1 > table > tbody > tr > td:nth-child(3)').text().slice(0, 10)
-                        var day = args[1]
-                        request('https://stu.goe.go.kr/sts_sci_md01_001.do?schulCode=' + code + '&&schulCrseScCode=3&schMmealScCode=2&schYmd=' + moment().format('YYYY.MM.DD'), function(error, response, html){
-                            var $ = cheerio.load(html, { decodeEntities: false });
-                            var meal = $('tbody > tr:nth-child(2)').children('td').eq(day)
-                            bot.sendMessage({ to: channelID, message: meal.html() })
-                        });
-                        // var obj = JSON.parse(fs.readFileSync('code.json', 'utf8'));
-                        obj[args[0]] = code;
-                        fs.writeFile('code.json', JSON.stringify(obj), function(err) {
-                            if (err) {
-                                console.log(err);
-                            }
-                        });                        
+                        getMeal(
+                            args[0], 
+                            $('.grid1 > table > tbody > tr > td:nth-child(3)').text().slice(0, 10), 
+                            day, channelID
+                        );                      
                     });
                 }
                 break;
             default:
-                bot.sendMessage({ to: channelID, message: '어, 아니야.' });
+                bot.sendMessage({ to: channelID, message: '그런 명령은 없어...' });
         }
     }
 })
